14f208eecb91978fbf0cdcaf328a28caba490bc7
Tim <>
2020-03-25 01:24:49 +1100

    Add git pull --rebase

--- conf.d/git.fish
+++ conf.d/git.fish
@@ -1,9 +1,15 @@
 abbr -a -U -- g git
+
 abbr -a -U -- gb 'git branch'
 abbr -a -U -- gbc 'git checkout -b'
-abbr -a -U -- gcm 'git commit --message'
 abbr -a -U -- gco 'git checkout'
-abbr -a -U -- gp 'git push'
+
 abbr -a -U -- gst 'git status'
+
+abbr -a -U -- gcm 'git commit --message'
 abbr -a -U -- gcf 'git commit --amend --reuse-message HEAD'
+
+abbr -a -U -- gpr 'git pull --rebase'
+
+abbr -a -U -- gp 'git push'
 abbr -a -U -- gpc 'git push --set-upstream origin "(git-branch-current 2> /dev/null)"'
