1c1ff96f7dfbb030f142ad0f4d71c3cb59111d44
Tim <>
2020-03-25 01:20:26 +1100

    Move more stuff to abbr

--- functions/gcf.fish
+++
@@ -1,3 +0,0 @@
-function gcf --wraps='git commit --amend --reuse-message HEAD' --description 'alias gcf=git commit --amend --reuse-message HEAD'
-  git commit --amend --reuse-message HEAD $argv;
-end
--- functions/gpc.fish
+++
@@ -1,3 +0,0 @@
-function gpc --wraps='git push --set-upstream origin "(git-branch-current 2> /dev/null)"' --description 'alias gpc=git push --set-upstream origin "(git-branch-current 2> /dev/null)"'
-  git push --set-upstream origin "(git-branch-current 2> /dev/null)" $argv;
-end
