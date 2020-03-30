fc3c3a251d635fd39e82e2ca8e58fc230519bc62
Tim <>
2020-03-30 13:19:36 +1100

    Fix gpc

--- conf.d/git.fish
+++ conf.d/git.fish
@@ -15,4 +15,4 @@ abbr -a -U -- gcf 'git commit --amend --reuse-message HEAD'
 abbr -a -U -- gfr 'git pull --rebase'
 
 abbr -a -U -- gp 'git push'
-abbr -a -U -- gpc 'git push --set-upstream origin "(git-current-branch 2> /dev/null)"'
+abbr -a -U -- gpc 'git push --set-upstream origin (git-current-branch 2> /dev/null)'
