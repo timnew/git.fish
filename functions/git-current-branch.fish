6cb16a4f28fa9bd4cf067c152a8671c75e4f1d46
Tim <>
2020-03-30 13:15:49 +1100

    Fix command

--- functions/git-current-branch.fish
+++ functions/git-current-branch.fish
@@ -2,7 +2,7 @@ function git-current-branch -d "Print out current git branch"
 
     set -l ref (command git symbolic-ref HEAD 2> /dev/null)
 
-    if [[ -n $ref ]]
+    if test -n $ref
         string replace "refs/heads/" "" $ref
         return 0
     else
