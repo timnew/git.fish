bad37dc4d9b8642c617eaf6fbe67a27904099184
Tim <>
2020-03-30 13:14:35 +1100

    add git-current-branch

---
+++ functions/git-current-branch.fish
@@ -0,0 +1,13 @@
+function git-current-branch -d "Print out current git branch"
+
+    set -l ref (command git symbolic-ref HEAD 2> /dev/null)
+
+    if [[ -n $ref ]]
+        string replace "refs/heads/" "" $ref
+        return 0
+    else
+        echo "ERROR: $PWD not a git repository" >&2
+        return 1
+    end
+
+end
